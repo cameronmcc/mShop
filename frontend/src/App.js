7bcbfc3eb4ab2cece647c11ac5a5320930929f13
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-04-02 08:57:40 -0700

    Header and Footer created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1>mShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to mShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App

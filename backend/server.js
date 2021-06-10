7a912eb0614a2417d4f64d409912caad3e0cf94b
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-06-10 11:51:54 -0700

    Now fetching products from the database successfully. Custom errors added.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
-// import { connect } from "mongoose"
 
 import productRoutes from "./routes/productRoutes.js"
 
@@ -17,6 +17,10 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 

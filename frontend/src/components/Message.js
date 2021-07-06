6dc5e1c7ebf68d5ea0e9bba89dfb9f27a9231423
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-07-05 21:21:32 -0600

    problem with dependncies successfully fixed.

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,9 +1,8 @@
 import React from "react"
 import { Alert } from "react-bootstrap"
 
-export const Message = ({ variant, children }) => {
-  return
-  ;<Alert variant={variant}>{children}</Alert>
+const Message = ({ variant, children }) => {
+  return <Alert variant={variant}>{children}</Alert>
 }
 
 Message.defaultProps = {
